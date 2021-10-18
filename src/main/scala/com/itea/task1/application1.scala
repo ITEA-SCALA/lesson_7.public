1227f88865b9dc1824c3e6bb17840fdf1a3803c5
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2021-10-18 15:03:40 +0300

    #master

--- src/main/scala/com/itea/task1/application1.scala
+++ src/main/scala/com/itea/task1/application1.scala
@@ -22,6 +22,7 @@ object Application1 extends App {
   }
   println( last(list, i => i-1) ) // 8
 
+  //  ( инфиксные и постфиксные операции в Scala )
   def lastRecursive[T](ls: List[T]): T = ls match {
     case h :: Nil => h              //TODO: [здесь инфиксная операция для работы со списком типа 'val l = 1 :: 2 :: 3 :: Nil'] а в случае когда дошли до крайнего элемента 'Nil', тогда в этом случае возвращаем его предшественника
     case _ :: t => lastRecursive(t) //TODO: [здесь инфиксная операция для работы со списком типа 'val l = 1 :: 2 :: 3 :: Nil'] в case получаем какой то список '_'  дальше вызываем метод next '::' следующий элемент списка...  sound because `next` is used only locally
